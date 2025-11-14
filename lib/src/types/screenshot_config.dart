b326ad2fe92f33ecbd30aae72336a2c89d6f7013
normidar <normidar7@gmail.com>
2025-11-14 19:12:12 +0900

    Update dependencies and version for release 0.5.0; upgraded flutter_riverpod to 3.0.0 and corrected README for overrides type.

--- lib/src/types/screenshot_config.dart
+++ lib/src/types/screenshot_config.dart
@@ -1,7 +1,7 @@
 import 'package:colaxy_screenshot/src/types/screenshot_page_info.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod/misc.dart';
 
 typedef EasyLocalizationWrapper = EasyLocalization Function(Widget);
 
